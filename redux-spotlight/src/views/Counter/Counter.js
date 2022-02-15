1db6d4f4fafc755402e5f68d8d0ae338e12b0a13
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-02-15 12:45:58 -0700

    Adds counter.js and button logic

--- redux-spotlight/src/views/Counter/Counter.js
+++ redux-spotlight/src/views/Counter/Counter.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './counterSlice';
 
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
